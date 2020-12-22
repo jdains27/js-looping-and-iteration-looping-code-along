411860605bbe0d1ade942ec9bafa6cc8cfcf33d3
jdains27 <dains.jordan2@gmail.com>
2020-12-22 14:04:59 -0600

    commit tree 6c4e151b4767863724ec70e622b2a9211c421f52

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let cards = (["Lisa", "Kaitlin", "Jan"], "suprise");
+    for(let i=0; i<names.length; i++) {
+        console.log('Thank you, ${names[i]}, for the wonderful ${event} gift!');
+    }
+    return cards;
+}
+
+function countDown(number) {
+    let i=0;
+    while(i<number +1) {
+        console.log(i++);
+    }
+}
